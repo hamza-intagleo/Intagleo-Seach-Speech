93b3d29b92ac8d0b4e91706753247906d2e5f6e3
HAMZA AZHAR <hamza@intagleo.com>
2019-03-04 10:11:38 +0500

    Rswg files

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -16,7 +16,7 @@ RSpec.configure do |config|
     'v1/swagger.json' => {
       swagger: '2.0',
       info: {
-        title: 'API V1',
+        title: 'Right Reply API',
         version: 'v1'
       },
       paths: {}
