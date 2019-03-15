02b45af6582e28819567db59bbc89f9f485d7688
zeeshan-Intagleo <zeeshan@intagleo.com>
2019-03-15 19:08:18 +0500

    Right reply admin page design

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+
+    // Toggle the side navigation
+  $("#sidebarToggle").on('click', function(e) {
+    e.preventDefault();
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+  });
+
+  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+    if ($(window).width() > 768) {
+      var e0 = e.originalEvent,
+        delta = e0.wheelDelta || -e0.detail;
+      this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+      e.preventDefault();
+    }
+  });
+  $('.sidebar').height($("#content-wrapper").height() + 25);
   $('.parsley-validate').parsley();
 
   $('body').on('click', '#sign_up', function(){
