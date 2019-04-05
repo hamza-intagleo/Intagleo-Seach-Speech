efa8a266c167e421502052f14f4599fca48de1f1
HAMZA AZHAR <hamza@intagleo.com>
2019-04-05 16:47:07 +0500

    free plan changes

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -5,20 +5,26 @@ class SubscriptionsController < ApplicationController
 
 
   def create_customer
+    unless current_user.has_free_plan?
     subscription = current_user.create_subscription!(params[:stripe_token], @plan.stripe_id)
-    if subscription[:customer].present?
-      customer = subscription[:customer]
-      current_user.save_subscription_details!( params[:stripe_token], 
-                                         customer['id'], 
-                                         customer['subscriptions']['data'].first['id'], 
-                                         customer['subscriptions']['data'].first['current_period_end'],
-                                         @plan.id,
-                                         true
-                                        )
-      location_path = (!current_user.is_plan_confirm ? users_pricing_path : (current_user.status == 'inactive' ? user_configuration_path(current_user) : user_dashboard_path(current_user, search_type: 'all'))) || root_path
-      return render :json => { :success => true, location_path: location_path}
+      if subscription[:customer].present?
+        customer = subscription[:customer]
+        current_user.save_subscription_details!( params[:stripe_token], 
+                                           customer['id'], 
+                                           customer['subscriptions']['data'].first['id'], 
+                                           customer['subscriptions']['data'].first['current_period_end'],
+                                           @plan.id,
+                                           true
+                                          )
+        location_path = (!current_user.is_plan_confirm ? users_pricing_path : (current_user.status == 'inactive' ? user_configuration_path(current_user) : user_dashboard_path(current_user, search_type: 'all'))) || root_path
+        return render :json => { :success => true, location_path: location_path}
+      else
+        return render :json => { :success => false, message: subscription[:message], location_path: users_pricing_path}
+      end
     else
-      return render :json => { :success => false, message: subscription[:message], location_path: users_pricing_path}
+      if current_user.update(is_plan_confirm: true)
+        redirect_to user_configuration_path(current_user)
+      end
     end  
   end
 
